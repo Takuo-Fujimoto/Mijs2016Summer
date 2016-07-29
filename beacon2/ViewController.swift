664dc9298e074cf7da2c74da67517b7fd61ff106
Takuo-Fujimoto <fw_ltt_beb@ezweb.ne.jp>
2016-07-29 14:13:51 +0900

    Initial Commit

---
+++ beacon2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  beacon2
+//
+//  Created by 藤本 拓央 on 2016/07/29.
+//  Copyright © 2016年 tac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
